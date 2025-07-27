54077f52ab012f236a3c43931a80457b9091d180
Aditya <adityajogdand15@gmail.com>
2025-07-27 17:18:27 +0530

    verfiy admin middle ware

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -1,1 +1,21 @@
-.
+const jwt  = require('jsonwebtoken')
+
+const verifyAdmin  = (req,res,next) => {
+    const token  = req.header('Authorization');
+
+    if(!token){
+        return res.status(401).json({message : "No token , Auth Denided by middle ware"})
+    }
+
+    try {
+        const decoded  = jwt.verify(token , process.env.JWT_SECRET);
+
+        req.user = decoded; 
+
+        next();
+    } catch (error) {
+        res.status(400).json({message: "Invalid Token , middleware "})
+    }
+}
+
+module.exports = verifyAdmin;
