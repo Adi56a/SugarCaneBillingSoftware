1c4b2175032fab7e15bd0373f153e6de4ca9e85e
Aditya <adityajogdand15@gmail.com>
2025-08-02 23:27:09 +0530

    farmer Routes updated

--- Backend/routes/farmerRoute.js
+++ Backend/routes/farmerRoute.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router  = express.Router()
-const {registerFarmer} = require('../controllers/farmerController')
+const {registerFarmer , fetchAllFarmer , fetchFarmerById} = require('../controllers/farmerController')
 const verifyAdmin = require('../middlewares/authMiddleware')
 
 
+
+
 router.post('/register', verifyAdmin ,registerFarmer)
+
+router.get('/all',fetchAllFarmer)
+router.get('/:selectedFarmerId',fetchFarmerById)
 
 module.exports = router;
